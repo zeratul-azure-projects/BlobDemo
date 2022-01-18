3e6c1547b7e77f8887ebe75d5d6fcde2fc85f4aa
ZerAtul7 <manekrafter@gmail.com>
2022-01-18 16:25:02 +0200

    Добавьте файлы проекта.

---
+++ BlobDemo/Models/FileModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlobDemo.Models
+{
+    public class FileModel
+    {
+        public IFormFile ImageFile { get; set; }
+    }
+}
---
+++ BlobDemo/Services/IFileManager.cs
@@ -0,0 +1,15 @@
+﻿using BlobDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlobDemo.Services
+{
+    public interface IFileManager
+    {
+        Task<byte[]> Get(string imageName);
+        Task Upload(FileModel model);
+        Task Delete(string imageName);
+    }
+}
